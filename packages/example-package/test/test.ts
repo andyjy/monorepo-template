0de242a59b4a807ddaceabf11399b0dff40d10f8
andyjy <andy@apexa.co.uk>
2021-10-29 18:28:53 +0100

    test

--- packages/example-package/test/test.ts
+++ packages/example-package/test/test.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@jest/globals";
 import main from "../src/index";
 
 test("echo", () => {
-  expect(main("echo")).toBe("echo");
+  expect(main("echo2")).toBe("echo2");
 });
